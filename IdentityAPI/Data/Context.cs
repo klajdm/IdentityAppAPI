c3a0ae3bc702ccbb4292500c37ba0a8fd828a5a0
klajdm <klajdimurati3@gmail.com>
2023-10-18 23:20:10 +0200

    added database migration

---
+++ IdentityAPI/Data/Context.cs
@@ -0,0 +1,14 @@
+﻿using IdentityAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityAPI.Data
+{
+    public class Context : IdentityDbContext<User>
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+            
+        }
+    }
+}
---
+++ IdentityAPI/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityAPI.Models
+{
+    public class User : IdentityUser
+    {
+        [Required]
+        public int FirsName { get; set; }
+        [Required]
+        public int LastName { get; set; }
+
+        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+    }
+}
