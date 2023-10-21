cbdda49075ecc83bf5cca8d53ad5d1a6d3bbd955
klajdm <klajdimurati3@gmail.com>
2023-10-21 22:59:07 +0200

    changes

---
+++ IdentityAPI/DTOs/Account/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace IdentityAPI.DTOs.Account
+{
+    public class UserDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string JWT { get; set; }
+    }
+}
